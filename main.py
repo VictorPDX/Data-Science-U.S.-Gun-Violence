ff041b976f477f058316032cbc4200446f50da0e
kououken <kououken@gmail.com>
2021-07-04 18:21:50 -0700

    Wrapped table name in quotes to allow for space.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ db_path = os.path.realpath(f'db/{db_name}.db')
 try:
     # load from Database
     conn = sql.connect(db_path)
-    DataFrame = pd.read_sql(f'SELECT * FROM {table_name}', conn)
+    DataFrame = pd.read_sql(f'SELECT * FROM "{table_name}"', conn)
 except (sql.OperationalError, pd.io.sql.DatabaseError):
     #  No table. Create the table, and get DataFrame representation
     DataFrame = pd.read_csv(data_path)
