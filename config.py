020f730136a479e357afe570e62bf4db3b01492c
Daniel Dupriest <kououken@gmail.com>
2021-07-18 15:31:36 -0700

    Added population csv data and modified load_db.py to create a 'population' table with states, years and populations.

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ db_name = "U.S. Gun Violence"
 table_name = "'Gun Violence'"
 data_path = 'data/stage3.csv'
 db_path = os.path.realpath(f'db/{db_name}.db')
+population_table_name = "population"
+population_data_path = "data/population_by_state.csv"
--- load db.py
+++ load db.py
@@ -1,11 +1,13 @@
+import csv
 import pandas as pd
 import sqlite3 as sql
 from config import *
 
+
 # VS Code: To view Table in Database, install SQLite extension, watch GIF examples
 
 
-
+# Load Gun Violence data
 try:
     # load from Database
     conn = sql.connect(db_path)
@@ -18,5 +20,37 @@ except (sql.OperationalError, pd.io.sql.DatabaseError):
 finally:
     conn.close()
 
+# Load state population data
+try:
+    conn = sql.connect(db_path)
+    cursor = conn.cursor()
+
+    # Recreate population table
+    cursor.execute(f"DROP TABLE IF EXISTS {population_table_name};")
+    print(f"Dropped table '{population_table_name}'.")
+    cursor.execute(f"""CREATE TABLE {population_table_name}(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    state TEXT NOT NULL,
+    year INTEGER NOT NULL,
+    POP INTEGER NOT NULL
+    )""")
+    print(f"Created table '{population_table_name}'.")
+
+    # Load data from csv
+    count = 0
+    input_file = csv.DictReader(open(population_data_path), delimiter=',')
+    for row in input_file:
+        # print(row)
+        for year in range(2010, 2020):
+            cursor.execute(f"""INSERT INTO {population_table_name}(state, year, pop)
+    VALUES("{row['state']}", {year}, {row[str(year)]})
+    """)
+            count += 1
+    print(f"Inserted {count} rows.")
+except Exception as ex:
+    print("Exception:", ex)
+finally:
+    conn.commit()
+    conn.close()
 
 print("DONE")
